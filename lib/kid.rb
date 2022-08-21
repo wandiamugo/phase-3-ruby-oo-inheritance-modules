ede5b7399a12edc53f6211e0d2523d7ec8835bd6
wandiamugo <wwandia173@gmail.com>
2022-08-21 12:27:29 +0300

    commit tree 045cf5f74e45f112f17bd835ecddbf02dca85e5d

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
 require_relative './dance_module'
 require_relative './meta_dancing_module'
+require_relative "./fancy_dance"
+
+class Kid
+  include Dance
+  extend MetaDancing
+  attr_accessor :name
+  extend FancyDance::ClassMethods
+  include FancyDance::InstanceMethods
+
+  def initialize(name)
+    @name = name
+  end
+end
